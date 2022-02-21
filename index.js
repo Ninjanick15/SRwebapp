979c22668882b7ba8211f58fbfd3c0519a20c296
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-02-21 10:31:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express'); //Set up the express module
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+const path = require('path')
+app.engine('pug', require('pug').__express)
+app.set('views', '.')
+app.set('view engine', 'pug')
+var fs = require("fs");
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 router.get('/', function(req, res){ 
   res.sendFile(path.join(__dirname, '/index.html'));
