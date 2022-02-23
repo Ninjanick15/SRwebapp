4e0f5c0e987a6bf4d1370b81f1834fce6604bd38
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-02-23 16:05:44 -0600

    trying to build app

---
+++ root-access/root-access/server.js
@@ -0,0 +1,28 @@
+const express = require('express'); //Set up the express module
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+const path = require('path')
+app.engine('pug', require('pug').__express)
+app.set('views', '.')
+app.set('view engine', 'pug')
+var fs = require("fs");
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+app.use('/', router);
+
+app.get('/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname, '/style.css')); //link the css
+});
+
+var img = require('path').join(__dirname, '/assets');
+app.use(express.static(img)); //link to images and other assets.
+
+//display html
+let server = app.listen(8080, function () {
+});
