d1981a9b3fa349a88ff5abe1be40364bc9623bc0
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-02-24 15:02:59 -0600

    database testing

--- script.js
+++ script.js
@@ -1,48 +1,65 @@
-const express = require('express'); //Set up the express module
-const app = express();
-const bodyParser = require('body-parser');
-const router = express.Router();
-const path = require('path')
-app.engine('pug', require('pug').__express)
-app.set('views', '.')
-app.set('view engine', 'pug')
-var fs = require("fs");
+const html = document.documentElement;
+const body = document.body;
+const menuLinks = document.querySelectorAll(".admin-menu a");
+const collapseBtn = document.querySelector(".admin-menu .collapse-btn");
+const toggleMobileMenu = document.querySelector(".toggle-mob-menu");
+const switchInput = document.querySelector(".switch input");
+const switchLabel = document.querySelector(".switch label");
+const switchLabelText = switchLabel.querySelector("span:last-child");
+const collapsedClass = "collapsed";
+const lightModeClass = "light-mode";
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//remoteSQL database connection
-var mysql = require('mysql');
-
-var con = mysql.createPool({ // .createPool if we want the connection to stay open(pool), .createConnection closes after a amount of time (normal)
-    host: "remotemysql.com", //do not change these four lines. login details for our DB
-    user: "mpelXDDRJY",
-    password: "r3KEzuSVtb",
-    database: "mpelXDDRJY"
+/*TOGGLE HEADER STATE*/
+collapseBtn.addEventListener("click", function () {
+    body.classList.toggle(collapsedClass);
+    this.getAttribute("aria-expanded") == "true"
+        ? this.setAttribute("aria-expanded", "false")
+        : this.setAttribute("aria-expanded", "true");
+    this.getAttribute("aria-label") == "collapse menu"
+        ? this.setAttribute("aria-label", "expand menu")
+        : this.setAttribute("aria-label", "collapse menu");
 });
 
-con.getConnection(function (err) { // .getConnection for pool connection, .connect for normal connection.
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log("Connected to database!");
+/*TOGGLE MOBILE MENU*/
+toggleMobileMenu.addEventListener("click", function () {
+    body.classList.toggle("mob-menu-opened");
+    this.getAttribute("aria-expanded") == "true"
+        ? this.setAttribute("aria-expanded", "false")
+        : this.setAttribute("aria-expanded", "true");
+    this.getAttribute("aria-label") == "open menu"
+        ? this.setAttribute("aria-label", "close menu")
+        : this.setAttribute("aria-label", "open menu");
 });
 
-//insert data into database
-
-app.post('/create_session', function (req, res) {
-
-    var datetime = req.body.datetime;
-    var envid = req.body.envid;
-    var numofleaves = req.body.numofleaves;
-    var height = req.body.height;
-
-
-    var sql = "INSERT INTO Growth (datetime, env_id, num_of_leaves, height) VALUES ('" + req.body.datetime + "','" + req.body.envid + "','" + req.body.numofleaves + "','" + req.body.height + "')";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        res.end();
+/*SHOW TOOLTIP ON MENU LINK HOVER*/
+for (const link of menuLinks) {
+    link.addEventListener("mouseenter", function () {
+        if (
+            body.classList.contains(collapsedClass) &&
+            window.matchMedia("(min-width: 768px)").matches
+        ) {
+            const tooltip = this.querySelector("span").textContent;
+            this.setAttribute("title", tooltip);
+        } else {
+            this.removeAttribute("title");
+        }
     });
-    res.send('<h3>Session Created!</h3><br> <button type="button"><a href="index.html">Back</a></button>');
+}
+
+/*TOGGLE LIGHT/DARK MODE*/
+if (localStorage.getItem("dark-mode") === "false") {
+    html.classList.add(lightModeClass);
+    switchInput.checked = false;
+    switchLabelText.textContent = "Light";
+}
+
+switchInput.addEventListener("input", function () {
+    html.classList.toggle(lightModeClass);
+    if (html.classList.contains(lightModeClass)) {
+        switchLabelText.textContent = "Light";
+        localStorage.setItem("dark-mode", "false");
+    } else {
+        switchLabelText.textContent = "Dark";
+        localStorage.setItem("dark-mode", "true");
+    }
 });
---
+++ server.js
@@ -0,0 +1,48 @@
+﻿const express = require('express'); //Set up the express module
+const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router();
+const path = require('path')
+app.engine('pug', require('pug').__express)
+app.set('views', '.')
+app.set('view engine', 'pug')
+var fs = require("fs");
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//remoteSQL database connection
+var mysql = require('mysql');
+
+var con = mysql.createPool({ // .createPool if we want the connection to stay open(pool), .createConnection closes after a amount of time (normal)
+    host: "remotemysql.com", //do not change these four lines. login details for our DB
+    user: "mpelXDDRJY",
+    password: "r3KEzuSVtb",
+    database: "mpelXDDRJY"
+});
+
+con.getConnection(function (err) { // .getConnection for pool connection, .connect for normal connection.
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log("Connected to database!");
+});
+
+//insert data into database
+
+app.post('/create_session', function (req, res) {
+
+    var datetime = req.body.datetime;
+    var envid = req.body.envid;
+    var numofleaves = req.body.numofleaves;
+    var height = req.body.height;
+
+
+    var sql = "INSERT INTO Growth (datetime, env_id, num_of_leaves, height) VALUES ('" + req.body.datetime + "','" + req.body.envid + "','" + req.body.numofleaves + "','" + req.body.height + "')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        res.end();
+    });
+    res.send('<h3>Session Created!</h3><br> <button type="button"><a href="index.html">Back</a></button>');
+});
