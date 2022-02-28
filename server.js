55b380e52b1578123af7acd90ce7d1730c8ef166
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-02-28 11:25:56 -0600

    push to get updated

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
 ﻿const express = require('express'); //Set up the express module
 const app = express();
 const bodyParser = require('body-parser');
-const router = express.Router();
-const path = require('path')
 app.engine('pug', require('pug').__express)
 app.set('views', '.')
 app.set('view engine', 'pug')
-var fs = require("fs");
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//remoteSQL database connection
-var mysql = require('mysql');
-
-var con = mysql.createPool({ // .createPool if we want the connection to stay open(pool), .createConnection closes after a amount of time (normal)
-    host: "remotemysql.com", //do not change these four lines. login details for our DB
-    user: "mpelXDDRJY",
-    password: "r3KEzuSVtb",
-    database: "mpelXDDRJY"
-});
-
-con.getConnection(function (err) { // .getConnection for pool connection, .connect for normal connection.
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log("Connected to database!");
-});
-
-//insert data into database
 
 app.post('/create_session', function (req, res) {
 
