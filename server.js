03dec34621811f5822794a072b5a46c7a58e2c5b
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-02-28 12:26:04 -0600

    heroku

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 ﻿const express = require('express'); //Set up the express module
 const app = express();
 const bodyParser = require('body-parser');
+const router = express.Router();
+const path = require('path')
 app.engine('pug', require('pug').__express)
 app.set('views', '.')
 app.set('view engine', 'pug')
+var fs = require("fs");
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.post('/create_session', function (req, res) {
-
-    var datetime = req.body.datetime;
-    var envid = req.body.envid;
-    var numofleaves = req.body.numofleaves;
-    var height = req.body.height;
-
-
-    var sql = "INSERT INTO Growth (datetime, env_id, num_of_leaves, height) VALUES ('" + req.body.datetime + "','" + req.body.envid + "','" + req.body.numofleaves + "','" + req.body.height + "')";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        res.end();
-    });
-    res.send('<h3>Session Created!</h3><br> <button type="button"><a href="index.html">Back</a></button>');
+//sends to index.html when visiting our link
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
+app.use('/', router);
