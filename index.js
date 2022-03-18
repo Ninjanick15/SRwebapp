c0d0fd2e54be67e38f627cd2818f08093db67f88
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-03-18 16:24:52 -0500

    update js

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ app.set('views', '.')
 app.set('view engine', 'pug')
 var fs = require("fs");
 
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
-
 //sends to index.html when visiting our link
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
