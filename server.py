8c5e29f05bd8cd1a40e1516bbeee0583146e337b
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-04-08 09:38:14 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,28 +2,25 @@ from __future__ import print_function
 from datetime import date, datetime, timedelta
 import mysql.connector
 
-cnx = mysql.connector.connect(
+rootdb = mysql.connector.connect(
     host='50.62.144.120',
     user='gljo4uxrniy6', 
     password='Wildcats1863',                        
     database='root-access'
     )
 
-cursor = cnx.cursor()
+cursor = rootdb.cursor()
 
-add_growth = ("INSERT INTO Growth "
-               "(datetime, env_id, num_of_leaves, height) "
-               "VALUES (%s, %s, %s, %s)")
+add_growth = ("INSERT INTO Growth (datetime, env_id, num_of_leaves, height) VALUES (%s, %s, %s, %s)")
 
-data_growth = (date(1977, 6, 14), '4', '3', '13')
+growth_data = (date(1977, 6, 14), '4', '3', '13')
 
 # Insert new growth
-cursor.execute(add_growth, data_growth)
-emp_no = cursor.lastrowid
+cursor.execute(add_growth, growth_data)
 
 # Make sure data is committed to the database
-cnx.commit()
+rootdb.commit()
 
 cursor.close()
 
-cnx.close()
+rootdb.close()
