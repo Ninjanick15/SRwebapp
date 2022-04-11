48b8221116040d49675e8d9b86b7014b25369af7
Ninjanick15 <88008070+Ninjanick15@users.noreply.github.com>
2022-04-11 09:38:17 -0500

    preparing for flask

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from datetime import date
 from datetime import date, datetime, timedelta
 import mysql.connector
 
@@ -12,30 +13,6 @@ rootdb = mysql.connector.connect(
     database='mpelXDDRJY'
     )
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/get-text', methods=['GET', 'POST'])
-def foo():
-    bar = request.form['test']
-
-if __name__ == '__main__':
-    app.run()
-
-# cursor = rootdb.cursor()
-
-# add_growth = ("INSERT INTO Growth (datetime, env_id, num_of_leaves, height) VALUES (%s, %s, %s, %s)")
-
-# growth_data = (date(1977, 6, 14), '4', '3', '13')
-
-# # Insert new growth
-# cursor.execute(add_growth, growth_data)
-
-# Make sure data is committed to the database
 rootdb.commit()
 
-# cursor.close()
-
 rootdb.close()
-
